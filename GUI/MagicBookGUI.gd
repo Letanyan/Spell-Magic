026936d4eac7e21e9fab05592ea35f93b3ea40ab
Letanyan Arumugam <letanyan.a@gmail.com>
2023-05-02 03:39:08 +0200

    gui save on text change

--- GUI/MagicBookGUI.gd
+++ GUI/MagicBookGUI.gd
@@ -50,12 +50,13 @@ func _on_spell_index_item_selected(index):
 	power_edit.text = "%f" % spell.power
 	duration_edit.text = "%f" % spell.duration
 	delay_edit.text = spell.delay
-	count_edit.text = "%f" % spell.count
+	count_edit.text = "%d" % spell.count
 	
 	element_edit.text = Spell.name_from_element(spell.element)
 	chain_edit.text = spell.chain.name if spell.chain else ""
 	is_rel.button_pressed = spell.is_relative_to_player_current_pos
 	is_bomb.button_pressed = spell.is_bomb
+	$container.visible = true
 
 
 func _on_save_pressed():
@@ -108,6 +109,8 @@ func _on_delete_pressed():
 	if current_index < 0:
 		return
 	book.spells.remove_at(current_index)
+	current_index = -1
+	$container.visible = false
 	reload_list()
 
 
@@ -117,3 +120,91 @@ func _on_create_pressed():
 	book.spells.append(spell)
 	reload_list()
 	_on_spell_index_item_selected(book.spells.size() - 1)
+	name_edit.grab_focus()
+	name_edit.select_all()
+
+
+func _on_name_edit_text_changed(new_text):
+	if current_index < 0:
+		return
+	book.spells[current_index].name = new_text
+	reload_list()
+
+func _on_element_text_changed(new_text):
+	if current_index < 0:
+		return
+	book.spells[current_index].element = Spell.element_from_name(new_text)
+
+func _on_x_text_changed(new_text):
+	if current_index < 0:
+		return
+	book.spells[current_index].x = new_text
+	book.spells[current_index].x_expr = Expr.new(new_text)
+
+
+func _on_y_text_changed(new_text):
+	if current_index < 0:
+		return
+	book.spells[current_index].y = new_text
+	book.spells[current_index].y_expr = Expr.new(new_text)
+
+func _on_z_text_changed(new_text):
+	if current_index < 0:
+		return
+	book.spells[current_index].z = new_text
+	book.spells[current_index].z_expr = Expr.new(new_text)
+
+func _on_r_text_changed(new_text):
+	if current_index < 0:
+		return
+	book.spells[current_index].r = new_text
+	book.spells[current_index].r_expr = Expr.new(new_text)
+
+func _on_N_text_changed(new_text):
+	if current_index < 0:
+		return
+	book.spells[current_index].count = new_text.to_int()
+
+func _on_P_text_changed(new_text):
+	if current_index < 0:
+		return
+	book.spells[current_index].power = new_text.to_float()
+
+func _on_T_text_changed(new_text):
+	if current_index < 0:
+		return
+	book.spells[current_index].duration = new_text.to_float()
+
+func _on_D_text_changed(new_text):
+	if current_index < 0:
+		return
+	book.spells[current_index].delay = new_text.to_float()
+	book.spells[current_index].d_expr = Expr.new(new_text)
+
+func _on_chain_text_changed(new_text):
+	if current_index < 0:
+		return
+	var spell = book.spells[current_index]
+	
+	var n = chain_edit.text
+	if n == "":
+		spell.chain = null
+	elif n != spell.name:
+		for s in book.spells:
+			if s.name == n:
+				spell.chain = s
+				
+	if spell.name != "":
+		for s in book.spells:
+			if s.chain != null and s.chain.name == spell.name and s.name != spell.name:
+				s.chain = spell
+
+func _on_is_rel_toggled(button_pressed):
+	if current_index < 0:
+		return
+	book.spells[current_index].is_relative_to_player_current_pos = button_pressed
+
+func _on_is_bomb_toggled(button_pressed):
+	if current_index < 0:
+		return
+	book.spells[current_index].is_bomb = button_pressed
