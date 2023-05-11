fb4dce4e909aaf3e0b2ffc1062f1b20273fff43b
Letanyan Arumugam <letanyan.a@gmail.com>
2023-05-11 21:10:16 +0200

    update electric and show fps

--- Worlds/Demo/demo.gd
+++ Worlds/Demo/demo.gd
@@ -52,6 +52,7 @@ func _ready():
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	$FPS.text = "FPS: " + str(Engine.get_frames_per_second())
 	pass
 	
 func _physics_process(delta):
