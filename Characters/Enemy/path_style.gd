c91ec4f85d0f7746019e33c9ff561005b2e75d13
Letanyan Arumugam <letanyan.a@gmail.com>
2024-05-22 01:43:01 +0200

    tweak when path movement is complete

--- Characters/Enemy/path_style.gd
+++ Characters/Enemy/path_style.gd
@@ -192,7 +192,9 @@ func next_position(delta: float, me: Enemy, player: Player, is_done: Globals.Ref
 	var result := Vector4(v.x, y, v.z, path.speed_at_time(time - delta, delta))
 	
 	#if fmod(time, path.total_duration) < fmod(old_t, path.total_duration):
-	if time >= path.total_duration and me.position.is_equal_approx(Vector3(v.x, y, v.z)):
+	
+	# don't use positions to determine completion as might get stuck if time near total_duration
+	if time >= path.total_duration: #and me.position.is_equal_approx(Vector3(v.x, y, v.z)):
 		me_start_position = null
 		time = 0.0
 		if is_done:
