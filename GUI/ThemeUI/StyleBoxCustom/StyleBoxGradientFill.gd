26a836d89f8e993535eecb1877c2361d331e8298
Letanyan Arumugam <letanyan.a@gmail.com>
2024-08-27 01:02:41 +0200

    optimise grad style box

--- GUI/ThemeUI/StyleBoxCustom/StyleBoxGradientFill.gd
+++ GUI/ThemeUI/StyleBoxCustom/StyleBoxGradientFill.gd
@@ -29,6 +29,7 @@ func _test_mask(point: Vector2, rect: Rect2) -> bool:
 	return rect.has_point(point)
 
 func render_rect(canvas_id: RID, rect: Rect2, texture: Texture2D) -> void:
+	#GDStyleMat.render_rect(canvas_id, rect, texture, Vector4(top_right_corner_radius, top_left_corner_radius, bottom_left_corner_radius, bottom_right_corner_radius), border_width, corner_radius_detail)
 	var vertices := PackedVector2Array([])
 	var uvs := PackedVector2Array([])
 	var colors := PackedColorArray([])
@@ -37,24 +38,28 @@ func render_rect(canvas_id: RID, rect: Rect2, texture: Texture2D) -> void:
 	var top_right_corner_uv := Vector2.ZERO
 	var top_right_corner_color := Color.WHITE
 	
+	var img: Image = null
+	if texture != null:
+		img = texture.get_image()
+	
 	if top_right_corner_radius != 0:
 		var center := rect.position + Vector2(rect.size.x - top_right_corner_radius, 0 + top_right_corner_radius)
 		var angle := 0.0
 		top_right_corner_vertex = center + Vector2(cos(angle) * top_right_corner_radius, -sin(angle) * top_right_corner_radius)
 		top_right_corner_uv = Vector2((top_right_corner_vertex.x - rect.position.x) / rect.size.x, (top_right_corner_vertex.y - rect.position.y) / rect.size.y)
-		top_right_corner_color = get_color(texture, top_right_corner_uv)
+		top_right_corner_color = get_color_from_image(img, top_right_corner_uv)
 		for i in top_right_corner_radius * corner_radius_detail + 1:
 			var point := center + Vector2(cos(angle) * top_right_corner_radius, -sin(angle) * top_right_corner_radius)
 			if vertices.is_empty() or vertices[vertices.size() - 1] != point:
 				vertices.append(point)
 				var u := Vector2((point.x - rect.position.x) / rect.size.x, (point.y - rect.position.y) / rect.size.y)
 				uvs.append(u)
-				colors.append(get_color(texture, u))
+				colors.append(get_color_from_image(img, u))
 			angle += (PI / 2.0) / (top_right_corner_radius * corner_radius_detail)
 	else:
 		top_right_corner_vertex = rect.position + Vector2(rect.size.x, 0)
 		top_right_corner_uv = Vector2(1, 0)
-		top_right_corner_color = get_color(texture, Vector2(1, 0))
+		top_right_corner_color = get_color_from_image(img, Vector2(1, 0))
 		vertices.append(top_right_corner_vertex)
 		uvs.append(top_right_corner_uv)
 		colors.append(top_right_corner_color)
@@ -68,12 +73,12 @@ func render_rect(canvas_id: RID, rect: Rect2, texture: Texture2D) -> void:
 				vertices.append(point)
 				var u := Vector2((point.x - rect.position.x) / rect.size.x, (point.y - rect.position.y) / rect.size.y)
 				uvs.append(u)
-				colors.append(get_color(texture, u))
+				colors.append(get_color_from_image(img, u))
 			angle += (PI / 2.0) / (top_left_corner_radius * corner_radius_detail)
 	else:
 		vertices.append(rect.position + Vector2(0, 0))
 		uvs.append(Vector2(0, 0))
-		colors.append(get_color(texture, Vector2(0, 0)))
+		colors.append(get_color_from_image(img, Vector2(0, 0)))
 		
 	if bottom_left_corner_radius != 0:
 		var center := rect.position + Vector2(0 + bottom_left_corner_radius, rect.size.y - bottom_left_corner_radius)
@@ -84,12 +89,12 @@ func render_rect(canvas_id: RID, rect: Rect2, texture: Texture2D) -> void:
 				vertices.append(point)
 				var u := Vector2((point.x - rect.position.x) / rect.size.x, (point.y - rect.position.y) / rect.size.y)
 				uvs.append(u)
-				colors.append(get_color(texture, u))
+				colors.append(get_color_from_image(img, u))
 			angle += (PI / 2.0) / (bottom_left_corner_radius * corner_radius_detail)
 	else:
 		vertices.append(rect.position + Vector2(0, rect.size.y))
 		uvs.append(Vector2(0, 1))
-		colors.append(get_color(texture, Vector2(0, 1)))
+		colors.append(get_color_from_image(img, Vector2(0, 1)))
 		
 	if bottom_right_corner_radius != 0:
 		var center := rect.position + Vector2(rect.size.x - bottom_right_corner_radius, rect.size.y - bottom_right_corner_radius)
@@ -100,12 +105,12 @@ func render_rect(canvas_id: RID, rect: Rect2, texture: Texture2D) -> void:
 				vertices.append(point)
 				var u := Vector2((point.x - rect.position.x) / rect.size.x, (point.y - rect.position.y) / rect.size.y)
 				uvs.append(u)
-				colors.append(get_color(texture, u))
+				colors.append(get_color_from_image(img, u))
 			angle += (PI / 2.0) / (bottom_right_corner_radius * corner_radius_detail)
 	else:
 		vertices.append(rect.position + Vector2(rect.size.x, rect.size.y))
 		uvs.append(Vector2(1, 1))
-		colors.append(get_color(texture, Vector2(1, 1)))
+		colors.append(get_color_from_image(img, Vector2(1, 1)))
 		
 	if vertices[vertices.size() - 1] != top_right_corner_vertex:
 		vertices.append(top_right_corner_vertex)
@@ -122,8 +127,12 @@ func get_color(tex: Texture2D, coord: Vector2) -> Color:
 	if tex != null:
 		var img := tex.get_image()
 		return img.get_pixel(roundi(coord.x * (img.get_width() - 1)), roundi(coord.y * (img.get_height() - 1)))
-		
 	return Color(1, 0, 0, 1)
+	
+func get_color_from_image(img: Image, coord: Vector2) -> Color:
+	if img != null:
+		return img.get_pixel(roundi(coord.x * (img.get_width() - 1)), roundi(coord.y * (img.get_height() - 1)))
+	return Color(0, 0, 0, 0)
 
 func set_border_gradient(start: Color, end: Color) -> void:
 	if border_texture is GradientTexture1D:
