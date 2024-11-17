74e84efc8fd435ae032c37a8819920264f78bea0
Letanyan Arumugam <letanyan.a@gmail.com>
2024-11-17 23:06:35 +0200

    buggy update chunk positions

--- Worlds/ChunkTest/chunk_test.gd
+++ Worlds/ChunkTest/chunk_test.gd
@@ -9,20 +9,22 @@ var chunker: Chunker
 func _ready() -> void:
 	var blender := NoiseBlender.version1(0)
 	chunker = Chunker.new(256, 0.0625, blender, [2, 8, 16])
-	chunker.set_world(get_world_3d())
+	chunker.set_world(self)
 
 func _physics_process(delta: float) -> void:
 	info.text = "%v" % camera.position
-
-func update_all_chunks() -> void:
-	for coord: Vector2i in chunker.chunk_rids:
-		var pos := chunker.chunk_positions[coord] as Vector2
-		var res := chunker.chunk_resolution / pow(2.0, chunker.vertex_indices[coord] as int)
-		chunker.update_chunk(coord, pos.x, pos.y, res)
+	
+	if chunker.has_chunks_to_update():
+		var updated := chunker.update_chunks_in_queue(Time.get_ticks_msec(), 5)
+		print(updated)
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		var ev := event as InputEventKey
+		if not ev.is_released():
+			return
 		match ev.keycode:
-			KEY_1:
-				update_all_chunks()
+			KEY_UP: chunker.update_chunks(Vector2i(0, 1))
+			KEY_DOWN: chunker.update_chunks(Vector2i(0, -1))
+			KEY_LEFT: chunker.update_chunks(Vector2i(-1, 0))
+			KEY_RIGHT: chunker.update_chunks(Vector2i(1, 0))
--- Worlds/Generator/Terrain/chunker.gd
+++ Worlds/Generator/Terrain/chunker.gd
@@ -7,10 +7,10 @@ var water_ripples_noise := preload("res://Worlds/SkyBox/ripples_noise.tres") as 
 var noise_texture := preload("res://Worlds/Generator/Terrain/noise_texture.tres") as NoiseTexture2D
 
 # keys are the LOD level. 1 << (value << 1) - (sum of all previous number of chunks) represents the number of chunks for this LOD level
-var lod_levels := {}
+var lod_levels: Array[int] = []
 var blender: NoiseBlender
 
-var vertex_indices := {} ## [Vector2i(coord)]int(LOD)
+var chunk_lods := {} ## [Vector2i(coord)]int(LOD)
 var chunk_rids := {} ## [Vector2i(coord)]RID(instance)
 var mesh_rids := {} ## [Vector2i(coord)]RID(mesh)
 var mats := {} ## [Vector2i(coord)]ShaderMaterial
@@ -24,6 +24,7 @@ var max_height_position := Vector3.ZERO
 
 var player_position := Vector2.ZERO
 var player_coord := Vector2i.ZERO
+var chunk_update_queue: Array[Dictionary] = []
 
 var chunk_width: float
 var chunk_resolution: float
@@ -32,18 +33,17 @@ func _init(_chunk_width: float, _chunk_resolution: float, _blender: NoiseBlender
 	var level := 0
 	chunk_width = _chunk_width
 	chunk_resolution = _chunk_resolution
+	lod_levels = lods
 	blender = _blender
 	var res := chunk_resolution
-	for value in lods:
-		lod_levels[level] = value
-		
-		print(level, ": res: ", res, " subdiv: ", res * chunk_width)
+	for value in lod_levels:
+		print(level, ": res: ", res, " subdiv: ", res * chunk_width, "(", -value, ", ", value - 1, ")")
 		for row in range(-value, value):
 			for col in range(-value, value):
 				var coord := Vector2i(col, row)
-				if vertex_indices.has(coord):
+				if chunk_lods.has(coord):
 					continue
-				vertex_indices[coord] = level
+				chunk_lods[coord] = level
 				var chunk := create_mesh(chunk_width, res)
 				var position := convert_coord_to_position(col, row)
 				chunk_rids[coord] = chunk[0]
@@ -57,7 +57,7 @@ func _init(_chunk_width: float, _chunk_resolution: float, _blender: NoiseBlender
 				if level == 0:
 					var body := create_static_body(chunk_width, res, map)
 					bodies[coord] = body
-				update_chunk(coord, position.x, position.y, res)
+				update_chunk(coord, coord, res)
 				
 		res *= 0.5
 		level += 1
@@ -66,10 +66,13 @@ func deinit() -> void:
 	for rid: RID in chunk_rids.values(): RenderingServer.free_rid(rid) 
 	for rid: RID in mesh_rids.values(): RenderingServer.free_rid(rid) 
 		
-func set_world(world: World3D) -> void:
+func set_world(world: Node3D) -> void:
 	for coord: Vector2i in chunk_rids:
 		var rid := chunk_rids[coord] as RID
-		RenderingServer.instance_set_scenario(rid, world.scenario)
+		RenderingServer.instance_set_scenario(rid, world.get_world_3d().scenario)
+		if bodies.has(coord):
+			var body := bodies[coord] as StaticBody3D
+			world.add_child(body)
 	
 func create_mesh(size: float, res: float) -> Array[RID]:
 	var plane := PlaneMesh.new()
@@ -102,13 +105,20 @@ func create_static_body(size: float, res: float, map: HeightMapShape3D) -> Stati
 	body.add_child(collision)
 	return body
 	
-func update_chunk(coord: Vector2i, x: float, z: float, res: float) -> void:
+func update_chunk(coord: Vector2i, new_coord: Vector2i, res: float) -> void:
 	var mesh := mesh_rids[coord] as RID
 	var mesh_data := RenderingServer.mesh_surface_get_arrays(mesh, 0)
 	var vertices := mesh_data[Mesh.ArrayType.ARRAY_VERTEX] as PackedVector3Array
+	if mesh_rids.has(new_coord):
+		var tmesh := mesh_rids[new_coord] as RID
+		var tmesh_data := RenderingServer.mesh_surface_get_arrays(tmesh, 0)
+		var tvertices := tmesh_data[Mesh.ArrayType.ARRAY_VERTEX] as PackedVector3Array
+		#print(res, ": ", coord, " -> ", new_coord, " __ ", vertices.size(), " -> ", tvertices.size())
 	var subdivide := subdivisions(res)
 	var R := chunk_width / (subdivide + 1)
 	var W := subdivide + 2
+	var x := new_coord.x * chunk_width
+	var z := new_coord.y * chunk_width
 	var X := x / R - W / 2.0
 	var Z := z / R - W / 2.0
 	X = snappedf(X, 1.0000)
@@ -123,6 +133,8 @@ func update_chunk(coord: Vector2i, x: float, z: float, res: float) -> void:
 	var S := 0.0001
 	if bodies.has(coord):
 		var static_body := bodies[coord] as StaticBody3D
+		var pos := new_coord * chunk_width
+		static_body.position = Vector3(pos.x, 0, pos.y)
 		var collision_shape := static_body.get_node("collision") as CollisionShape3D
 		var hmap := collision_shape.shape as HeightMapShape3D
 		var array := PackedFloat32Array()
@@ -174,8 +186,112 @@ func update_chunk(coord: Vector2i, x: float, z: float, res: float) -> void:
 	mat.set_shader_parameter("locations", blender.back.get_locations())
 	RenderingServer.mesh_surface_set_material(mesh, 0, mat)
 	
-	RenderingServer.instance_set_transform(chunk_rids[coord] as RID, T.I.translated(Vector3(x, 0, z)))
+	var rid := chunk_rids[coord] as RID
+	RenderingServer.instance_set_transform(rid, T.I.translated(Vector3(x, 0, z)))
 	
+func has_chunks_to_update() -> bool:
+	return not chunk_update_queue.is_empty()
+	
+func update_chunks_in_queue(start: int, limit: int) -> Array[Vector3i]:
+	var result: Array[Vector3i] = []
+	var duration := Time.get_ticks_msec() - start
+	var index := chunk_update_queue.size() - 1
+	while duration < limit and index >= 0:
+		var params := chunk_update_queue[index]
+		index -= 1
+		var coord0 := params["coord0"] as Vector2i
+		var coord1 := params["coord1"] as Vector2i
+		var res0 := params["res0"] as float
+		update_chunk(coord0, coord1, res0)
+		result.append(Vector3i(coord0.x, coord0.y, roundi(chunk_resolution / res0 - 1)))
+		
+		if params.has("res1"):
+			var res1 := params["res1"] as float
+			update_chunk(coord1, coord0, res1)
+			result.append(Vector3i(coord1.x, coord1.y, roundi(chunk_resolution / res1 - 1)))
+			swap_keys(chunk_lods, coord0, coord1)
+			swap_keys(chunk_rids, coord0, coord1)
+			swap_keys(mesh_rids, coord0, coord1)
+			swap_keys(mats, coord0, coord1)
+			swap_keys(chunk_positions, coord0, coord1)
+			if bodies.has(coord0) and bodies.has(coord1):
+				swap_keys(bodies, coord0, coord1)
+			elif bodies.has(coord0):
+				move_key(bodies, coord0, coord1)
+			elif bodies.has(coord1):
+				move_key(bodies, coord1, coord0)
+			swap_keys(height_maps, coord0, coord1)
+		else:
+			move_key(chunk_lods, coord0, coord1)
+			move_key(chunk_rids, coord0, coord1)
+			move_key(mesh_rids, coord0, coord1)
+			move_key(mats, coord0, coord1)
+			move_key(chunk_positions, coord0, coord1)
+			if bodies.has(coord0):
+				move_key(bodies, coord0, coord1)
+			move_key(height_maps, coord0, coord1)
+		
+		duration = Time.get_ticks_msec() - start
+		
+	chunk_update_queue.resize(index + 1)
+	return result	
+	
+func swap_keys(dict: Dictionary, key0: Variant, key1: Variant) -> void:
+	var temp: Variant = dict[key0]
+	dict[key0] = dict[key1]
+	dict[key1] = temp
+	
+func move_key(dict: Dictionary, from: Variant, to: Variant) -> void:
+	dict[to] = dict[from]
+	dict.erase(from)
+	
+func queue_contains(vec: Vector2i) -> int:
+	for dict in chunk_update_queue:
+		if (dict["coord0"] as Vector2i) == vec:
+			return 0
+		if (dict["coord1"] as Vector2i) == vec:
+			return 1
+	return -1
+	
+func update_chunks(delta: Vector2i) -> void:
+	for lod in lod_levels.size():
+		var to_flip := edges_at_direction(lod, delta * -1)
+		var into := edges_at_direction(lod, delta)
+		var value := lod_levels[lod]
+		for i in to_flip.size():
+			if lod + 1 < lod_levels.size():
+				var dict := {"coord0": to_flip[i], "coord1": into[i] + delta, "res0": resoultion(lod) , "res1": resoultion(lod + 1)}
+				var find := queue_contains(to_flip[i])
+				if find != -1: 
+					print()
+				find = queue_contains(into[i] + delta)
+				if find != -1: 
+					print()
+				chunk_update_queue.append(dict)
+			else:
+				var dict := {"coord0": to_flip[i], "coord1": into[i] + delta, "res0": resoultion(lod)}
+				var find := queue_contains(to_flip[i])
+				if find != -1: 
+					print()
+				find = queue_contains(into[i] + delta)
+				if find != -1: 
+					print()
+				chunk_update_queue.append(dict)
+				
+	player_coord += delta
+	
+func edges_at_direction(lod: int, direction: Vector2i) -> Array[Vector2i]:
+	var value := lod_levels[lod] as int
+	var result: Array[Vector2i] = []
+	if direction.x == -1:
+		for i in range(-value, value): result.append(Vector2i(-value + player_coord.x, i + player_coord.y))
+	if direction.x == 1:
+		for i in range(-value, value): result.append(Vector2i(value - 1 + player_coord.x, i + player_coord.y))
+	if direction.y == -1:
+		for i in range(-value, value): result.append(Vector2i(i + player_coord.x, -value + player_coord.y))
+	if direction.y == 1:
+		for i in range(-value, value): result.append(Vector2i(i + player_coord.x, value - 1 + player_coord.y))
+	return result
 	
 func convert_position_to_coord(x: float, z: float) -> Vector2i:
 	return Vector2i(floori(x / chunk_width), floori(z / chunk_width))
@@ -185,3 +301,6 @@ func convert_coord_to_position(x: float, z: float) -> Vector2:
 
 func subdivisions(res: float) -> int:
 	return floori(chunk_width * res) - 1
+
+func resoultion(lod: int) -> float:
+	return chunk_resolution / pow(2, lod)
