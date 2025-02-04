b5a3eff242e0d9e7da393a3e63268c4d096d7fa4
Letanyan Arumugam <letanyan.a@gmail.com>
2025-02-04 15:40:42 +0200

    new player model
    - fix drawing water surface when under water

--- GUI/Pause Menu/StatsView.gd
+++ GUI/Pause Menu/StatsView.gd
@@ -29,11 +29,6 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
-	var text := "-unit(posi)"
-	var expr := Expr.new(text)
-	var vars := Vars.new()
-	vars.set_raw("posi", Vector3(1, 2, 3))
-	print(text, " = ", expr.compute(vars, true))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
