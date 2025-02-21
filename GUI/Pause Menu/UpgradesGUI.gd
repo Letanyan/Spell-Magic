36a64165563fb1056868c99a682f868c4b3e6153
Letanyan Arumugam <letanyan.a@gmail.com>
2025-02-21 18:23:48 +0200

    display maxed upgrades

--- GUI/Pause Menu/UpgradesGUI.gd
+++ GUI/Pause Menu/UpgradesGUI.gd
@@ -106,6 +106,30 @@ func update_state(purchase_error: UpgradeSettings.PurchaseError) -> void:
 		popup.confirmed.connect(func() -> void: UIAudioPlayer.click())
 		popup.show_in_root(self)
 		return
+		
+	const coin_suffix := " [img color=#ffc000]res://GUI/Images/coins.svg[/img][/center]"
+	currency.text = "[right]" + str(settings.upgrade_settings.currency) + " [img color=#ffc000]res://GUI/Images/coins.svg[/img][/right]"
+	
+	element_void_upgrade.disabled = settings.upgrade_settings.check_if_has_spell_element(Spell.Element.VOID) or settings.upgrade_settings.currency < settings.upgrade_settings.cost_spell_element
+	element_fire_upgrade.disabled = settings.upgrade_settings.check_if_has_spell_element(Spell.Element.FIRE) or settings.upgrade_settings.currency < settings.upgrade_settings.cost_spell_element
+	element_water_upgrade.disabled = settings.upgrade_settings.check_if_has_spell_element(Spell.Element.WATER) or settings.upgrade_settings.currency < settings.upgrade_settings.cost_spell_element
+	element_air_upgrade.disabled = settings.upgrade_settings.check_if_has_spell_element(Spell.Element.AIR) or settings.upgrade_settings.currency < settings.upgrade_settings.cost_spell_element
+	element_rock_upgrade.disabled = settings.upgrade_settings.check_if_has_spell_element(Spell.Element.ROCK) or settings.upgrade_settings.currency < settings.upgrade_settings.cost_spell_element
+	element_ice_upgrade.disabled = settings.upgrade_settings.check_if_has_spell_element(Spell.Element.ICE) or settings.upgrade_settings.currency < settings.upgrade_settings.cost_spell_element
+	element_electric_upgrade.disabled = settings.upgrade_settings.check_if_has_spell_element(Spell.Element.ELECTRIC) or settings.upgrade_settings.currency < settings.upgrade_settings.cost_spell_element
+	chain_at_start_upgrade.disabled = settings.upgrade_settings.check_if_has_chain_method(Spell.ChainCastKind.START) or settings.upgrade_settings.currency < settings.upgrade_settings.cost_chain_method(Spell.ChainCastKind.START)
+	chain_at_end_upgrade.disabled = settings.upgrade_settings.check_if_has_chain_method(Spell.ChainCastKind.END) or settings.upgrade_settings.currency < settings.upgrade_settings.cost_chain_method(Spell.ChainCastKind.END)
+	chain_on_hit_upgrade.disabled = settings.upgrade_settings.check_if_has_chain_method(Spell.ChainCastKind.HIT) or settings.upgrade_settings.currency < settings.upgrade_settings.cost_chain_method(Spell.ChainCastKind.HIT)
+	element_void_cost.text = "[center]" + str(settings.upgrade_settings.cost_spell_element) + coin_suffix if not settings.upgrade_settings.check_if_has_spell_element(Spell.Element.VOID) else "[center][font_size=12]UNLOCKED[/font_size][/center]"
+	element_fire_cost.text = "[center]" + str(settings.upgrade_settings.cost_spell_element) + coin_suffix if not settings.upgrade_settings.check_if_has_spell_element(Spell.Element.FIRE) else "[center][font_size=12]UNLOCKED[/font_size][/center]"
+	element_water_cost.text = "[center]" + str(settings.upgrade_settings.cost_spell_element) + coin_suffix if not settings.upgrade_settings.check_if_has_spell_element(Spell.Element.WATER) else "[center][font_size=12]UNLOCKED[/font_size][/center]"
+	element_air_cost.text = "[center]" + str(settings.upgrade_settings.cost_spell_element) + coin_suffix if not settings.upgrade_settings.check_if_has_spell_element(Spell.Element.AIR) else "[center][font_size=12]UNLOCKED[/font_size][/center]"
+	element_rock_cost.text = "[center]" + str(settings.upgrade_settings.cost_spell_element) + coin_suffix if not settings.upgrade_settings.check_if_has_spell_element(Spell.Element.ROCK) else "[center][font_size=12]UNLOCKED[/font_size][/center]"
+	element_ice_cost.text = "[center]" + str(settings.upgrade_settings.cost_spell_element) + coin_suffix if not settings.upgrade_settings.check_if_has_spell_element(Spell.Element.ICE) else "[center][font_size=12]UNLOCKED[/font_size][/center]"
+	element_electric_cost.text = "[center]" + str(settings.upgrade_settings.cost_spell_element) + coin_suffix if not settings.upgrade_settings.check_if_has_spell_element(Spell.Element.ELECTRIC) else "[center][font_size=12]UNLOCKED[/font_size][/center]"
+	chain_at_start_cost.text = "[center]" + str(settings.upgrade_settings.cost_chain_method(Spell.ChainCastKind.START) ) + coin_suffix if not settings.upgrade_settings.check_if_has_chain_method(Spell.ChainCastKind.START) else "[center][font_size=12]UNLOCKED[/font_size][/center]"
+	chain_at_end_cost.text = "[center]" + str(settings.upgrade_settings.cost_chain_method(Spell.ChainCastKind.END)) + coin_suffix if not settings.upgrade_settings.check_if_has_chain_method(Spell.ChainCastKind.END) else "[center][font_size=12]UNLOCKED[/font_size][/center]"
+	chain_on_hit_cost.text = "[center]" + str(settings.upgrade_settings.cost_chain_method(Spell.ChainCastKind.HIT)) + coin_suffix if not settings.upgrade_settings.check_if_has_chain_method(Spell.ChainCastKind.HIT) else "[center][font_size=12]UNLOCKED[/font_size][/center]"
 	
 	max_spell_count_current.text = str(settings.upgrade_settings.max_spells_in_book())
 	max_running_speed_current.text = str(settings.upgrade_settings.max_running_speed()) + "m/s"
@@ -121,50 +145,6 @@ func update_state(purchase_error: UpgradeSettings.PurchaseError) -> void:
 	mana_regen_current.text = str(settings.upgrade_settings.max_mana_regen())
 	max_cr_current.text = str(settings.upgrade_settings.max_crit_rate()) + "%"
 	max_cd_current.text = str(settings.upgrade_settings.max_crit_dmg())
-	
-	# TODO: grey out maxed upgrades
-	element_void_upgrade.disabled = settings.upgrade_settings.check_if_has_spell_element(Spell.Element.VOID) or settings.upgrade_settings.currency < settings.upgrade_settings.cost_spell_element
-	element_fire_upgrade.disabled = settings.upgrade_settings.check_if_has_spell_element(Spell.Element.FIRE) or settings.upgrade_settings.currency < settings.upgrade_settings.cost_spell_element
-	element_water_upgrade.disabled = settings.upgrade_settings.check_if_has_spell_element(Spell.Element.WATER) or settings.upgrade_settings.currency < settings.upgrade_settings.cost_spell_element
-	element_air_upgrade.disabled = settings.upgrade_settings.check_if_has_spell_element(Spell.Element.AIR) or settings.upgrade_settings.currency < settings.upgrade_settings.cost_spell_element
-	element_rock_upgrade.disabled = settings.upgrade_settings.check_if_has_spell_element(Spell.Element.ROCK) or settings.upgrade_settings.currency < settings.upgrade_settings.cost_spell_element
-	element_ice_upgrade.disabled = settings.upgrade_settings.check_if_has_spell_element(Spell.Element.ICE) or settings.upgrade_settings.currency < settings.upgrade_settings.cost_spell_element
-	element_electric_upgrade.disabled = settings.upgrade_settings.check_if_has_spell_element(Spell.Element.ELECTRIC) or settings.upgrade_settings.currency < settings.upgrade_settings.cost_spell_element
-	
-	chain_at_start_upgrade.disabled = settings.upgrade_settings.check_if_has_chain_method(Spell.ChainCastKind.START) or settings.upgrade_settings.currency < settings.upgrade_settings.cost_chain_method(Spell.ChainCastKind.START)
-	chain_at_end_upgrade.disabled = settings.upgrade_settings.check_if_has_chain_method(Spell.ChainCastKind.END) or settings.upgrade_settings.currency < settings.upgrade_settings.cost_chain_method(Spell.ChainCastKind.END)
-	chain_on_hit_upgrade.disabled = settings.upgrade_settings.check_if_has_chain_method(Spell.ChainCastKind.HIT) or settings.upgrade_settings.currency < settings.upgrade_settings.cost_chain_method(Spell.ChainCastKind.HIT)
-	
-	var coin_suffix := " [img color=#ffc000]res://GUI/Images/coins.svg[/img][/center]"
-	max_spell_count_cost.text = "[center]" + str(settings.upgrade_settings.cost_spells_in_book()) + coin_suffix
-	max_running_speed_cost.text = "[center]" + str(settings.upgrade_settings.cost_running_speed()) + coin_suffix
-	max_P_cost.text = "[center]" + str(settings.upgrade_settings.cost_P()) + coin_suffix
-	max_v_cost.text = "[center]" + str(settings.upgrade_settings.cost_v()) + coin_suffix
-	max_N_cost.text = "[center]" + str(settings.upgrade_settings.cost_N()) + coin_suffix
-	max_T_cost.text = "[center]" + str(settings.upgrade_settings.cost_T()) + coin_suffix
-	max_M_cost.text = "[center]" + str(settings.upgrade_settings.cost_mana()) + coin_suffix
-	max_r_cost.text = "[center]" + str(settings.upgrade_settings.cost_r()) + coin_suffix
-	max_H_cost.text = "[center]" + str(settings.upgrade_settings.cost_health()) + coin_suffix
-	attack_cost.text = "[center]" + str(settings.upgrade_settings.cost_attack()) + coin_suffix
-	defence_cost.text = "[center]" + str(settings.upgrade_settings.cost_defence()) + coin_suffix
-	mana_regen_cost.text = "[center]" + str(settings.upgrade_settings.cost_mana_regen()) + coin_suffix
-	max_cr_cost.text = "[center]" + str(settings.upgrade_settings.cost_crit_rate()) + coin_suffix
-	max_cd_cost.text = "[center]" + str(settings.upgrade_settings.cost_crit_dmg()) + coin_suffix
-	
-	element_void_cost.text = "[center]" + str(settings.upgrade_settings.cost_spell_element) + coin_suffix
-	element_fire_cost.text = "[center]" + str(settings.upgrade_settings.cost_spell_element) + coin_suffix
-	element_water_cost.text = "[center]" + str(settings.upgrade_settings.cost_spell_element) + coin_suffix
-	element_air_cost.text = "[center]" + str(settings.upgrade_settings.cost_spell_element) + coin_suffix
-	element_rock_cost.text = "[center]" + str(settings.upgrade_settings.cost_spell_element) + coin_suffix
-	element_ice_cost.text = "[center]" + str(settings.upgrade_settings.cost_spell_element) + coin_suffix
-	element_electric_cost.text = "[center]" + str(settings.upgrade_settings.cost_spell_element) + coin_suffix
-	
-	chain_at_start_cost.text = "[center]" + str(settings.upgrade_settings.cost_chain_method(Spell.ChainCastKind.START) ) + coin_suffix
-	chain_at_end_cost.text = "[center]" + str(settings.upgrade_settings.cost_chain_method(Spell.ChainCastKind.END)) + coin_suffix
-	chain_on_hit_cost.text = "[center]" + str(settings.upgrade_settings.cost_chain_method(Spell.ChainCastKind.HIT)) + coin_suffix
-	
-	currency.text = "[right]" + str(settings.upgrade_settings.currency) + " [img color=#ffc000]res://GUI/Images/coins.svg[/img][/right]"
-	
 	max_spell_count_upgrade.disabled = settings.upgrade_settings.level_spells_in_book >= UpgradeSettings.level_max_spells_in_book or settings.upgrade_settings.currency < settings.upgrade_settings.cost_spells_in_book()
 	max_running_speed_upgrade.disabled = settings.upgrade_settings.level_running_speed >= UpgradeSettings.level_max_running_speed or settings.upgrade_settings.currency < settings.upgrade_settings.cost_running_speed()
 	max_P_upgrade.disabled = settings.upgrade_settings.level_P >= UpgradeSettings.level_max_P or settings.upgrade_settings.currency < settings.upgrade_settings.cost_P()
@@ -179,21 +159,34 @@ func update_state(purchase_error: UpgradeSettings.PurchaseError) -> void:
 	mana_regen_upgrade.disabled = settings.upgrade_settings.level_mana_regen >= UpgradeSettings.level_max_mana_regen or settings.upgrade_settings.currency < settings.upgrade_settings.cost_mana_regen()
 	max_cr_upgrade.disabled = settings.upgrade_settings.level_crit_rate >= UpgradeSettings.level_max_crit_rate or settings.upgrade_settings.currency < settings.upgrade_settings.cost_crit_rate()
 	max_cd_upgrade.disabled = settings.upgrade_settings.level_crit_dmg >= UpgradeSettings.level_max_crit_dmg or settings.upgrade_settings.currency < settings.upgrade_settings.cost_crit_dmg()
-	
-	max_spell_count_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_spells_in_book())
-	max_running_speed_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_running_speed()) + "m/s"
-	max_P_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_P())
-	max_v_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_v()) + "m/s"
-	max_T_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_T()) + "s"
-	max_N_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_N())
-	max_H_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_health())
-	max_M_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_mana())
-	max_r_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_r()) + "m"
-	attack_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_attack())
-	defence_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_defence())
-	mana_regen_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_mana_regen())
-	max_cr_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_crit_rate()) + "%"
-	max_cd_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_crit_dmg())
+	max_spell_count_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_spells_in_book()) if settings.upgrade_settings.level_spells_in_book < UpgradeSettings.level_max_spells_in_book else "MAXED"
+	max_running_speed_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_running_speed()) + "m/s" if settings.upgrade_settings.level_running_speed < UpgradeSettings.level_max_running_speed else "MAXED"
+	max_P_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_P()) if settings.upgrade_settings.level_P < UpgradeSettings.level_max_P else "MAXED"
+	max_v_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_v()) + "m/s" if settings.upgrade_settings.level_v < UpgradeSettings.level_max_v else "MAXED"
+	max_T_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_T()) + "s" if settings.upgrade_settings.level_T < UpgradeSettings.level_max_T else "MAXED"
+	max_N_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_N()) if settings.upgrade_settings.level_N < UpgradeSettings.level_max_N else "MAXED"
+	max_H_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_health()) if settings.upgrade_settings.level_health < UpgradeSettings.level_max_health else "MAXED"
+	max_M_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_mana()) if settings.upgrade_settings.level_mana < UpgradeSettings.level_max_mana else "MAXED"
+	max_r_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_r()) + "m" if settings.upgrade_settings.level_r < UpgradeSettings.level_max_r else "MAXED"
+	attack_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_attack()) if settings.upgrade_settings.level_attack < UpgradeSettings.level_max_attack else "MAXED"
+	defence_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_defence()) if settings.upgrade_settings.level_defence < UpgradeSettings.level_max_defence else "MAXED"
+	mana_regen_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_mana_regen()) if settings.upgrade_settings.level_mana_regen < UpgradeSettings.level_max_mana_regen else "MAXED"
+	max_cr_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_crit_rate()) + "%" if settings.upgrade_settings.level_crit_rate < UpgradeSettings.level_max_crit_rate else "MAXED"
+	max_cd_upgrade.text = "+ " + str(settings.upgrade_settings.upgrade_crit_dmg()) if settings.upgrade_settings.level_crit_dmg < UpgradeSettings.level_max_crit_dmg else "MAXED"
+	max_spell_count_cost.text = "[center]" + str(settings.upgrade_settings.cost_spells_in_book()) + coin_suffix if settings.upgrade_settings.level_spells_in_book < UpgradeSettings.level_max_spells_in_book else ""
+	max_running_speed_cost.text = "[center]" + str(settings.upgrade_settings.cost_running_speed()) + coin_suffix if settings.upgrade_settings.level_running_speed < UpgradeSettings.level_max_running_speed else ""
+	max_P_cost.text = "[center]" + str(settings.upgrade_settings.cost_P()) + coin_suffix if settings.upgrade_settings.level_P < UpgradeSettings.level_max_P else ""
+	max_v_cost.text = "[center]" + str(settings.upgrade_settings.cost_v()) + coin_suffix if settings.upgrade_settings.level_v < UpgradeSettings.level_max_v else ""
+	max_N_cost.text = "[center]" + str(settings.upgrade_settings.cost_N()) + coin_suffix if settings.upgrade_settings.level_T < UpgradeSettings.level_max_T else ""
+	max_T_cost.text = "[center]" + str(settings.upgrade_settings.cost_T()) + coin_suffix if settings.upgrade_settings.level_N < UpgradeSettings.level_max_N else ""
+	max_M_cost.text = "[center]" + str(settings.upgrade_settings.cost_mana()) + coin_suffix if settings.upgrade_settings.level_health < UpgradeSettings.level_max_health else ""
+	max_r_cost.text = "[center]" + str(settings.upgrade_settings.cost_r()) + coin_suffix if settings.upgrade_settings.level_mana < UpgradeSettings.level_max_mana else ""
+	max_H_cost.text = "[center]" + str(settings.upgrade_settings.cost_health()) + coin_suffix if settings.upgrade_settings.level_r < UpgradeSettings.level_max_r else ""
+	attack_cost.text = "[center]" + str(settings.upgrade_settings.cost_attack()) + coin_suffix if settings.upgrade_settings.level_attack < UpgradeSettings.level_max_attack else ""
+	defence_cost.text = "[center]" + str(settings.upgrade_settings.cost_defence()) + coin_suffix if settings.upgrade_settings.level_defence < UpgradeSettings.level_max_defence else ""
+	mana_regen_cost.text = "[center]" + str(settings.upgrade_settings.cost_mana_regen()) + coin_suffix if settings.upgrade_settings.level_mana_regen < UpgradeSettings.level_max_mana_regen else ""
+	max_cr_cost.text = "[center]" + str(settings.upgrade_settings.cost_crit_rate()) + coin_suffix if settings.upgrade_settings.level_crit_rate < UpgradeSettings.level_max_crit_rate else ""
+	max_cd_cost.text = "[center]" + str(settings.upgrade_settings.cost_crit_dmg()) + coin_suffix if settings.upgrade_settings.level_crit_dmg < UpgradeSettings.level_max_crit_dmg else ""
 
 func _on_max_spell_count_upgrade_pressed() -> void:
 	var err := settings.upgrade_settings.purchase_spells_in_book()
